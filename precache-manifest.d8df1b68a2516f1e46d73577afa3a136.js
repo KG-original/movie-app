c65570a751c69aadf1a95ae52763f2863255c8ba
KG-original <kgalange@gmail.com>
2019-09-08 06:04:34 +0200

    Updates

---
+++ precache-manifest.d8df1b68a2516f1e46d73577afa3a136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d975d0f964781f08ecf4702c68cd60",
+    "url": "/KG-original/movie-app/index.html"
+  },
+  {
+    "revision": "58742bf46a24543e3aa4",
+    "url": "/KG-original/movie-app/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "74bebce76d7fab6826a6",
+    "url": "/KG-original/movie-app/static/css/main.6c2b8b99.chunk.css"
+  },
+  {
+    "revision": "58742bf46a24543e3aa4",
+    "url": "/KG-original/movie-app/static/js/2.fd1c1807.chunk.js"
+  },
+  {
+    "revision": "74bebce76d7fab6826a6",
+    "url": "/KG-original/movie-app/static/js/main.3d52af9d.chunk.js"
+  },
+  {
+    "revision": "d447cc0bd154bd952b8d",
+    "url": "/KG-original/movie-app/static/js/runtime~main.3c768d5c.js"
+  }
+]);
